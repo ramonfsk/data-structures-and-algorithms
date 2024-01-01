91113f2214ac3f8c88bf6641e7b554af3b0bef59
Ramon Ferreira <ramonfsk@gmail.com>
2024-01-01 11:01:31 -0300

    solve using two-pointers approach by IA

--- array/MinimumDifferenceProblem/MinimumDifferenceProblem/main.cpp
+++ array/MinimumDifferenceProblem/MinimumDifferenceProblem/main.cpp
@@ -36,6 +36,37 @@ pair<int, int> minimumDifference(vector<int> a, vector<int> b) {
     return result;
 }
 
+pair<int, int> minimumDifferenceUsingTwoPointers(vector<int> a, vector<int> b) {
+    // sort arrays
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    pair<int, int> result;
+    int minDifference = INT_MAX;
+
+    int i = 0; // pointer for array 'a'
+    int j = 0; // pointer for array 'b'
+
+    while (i < a.size() && j < b.size()) {
+        int currentMinDifference = abs(a[i] - b[j]);
+
+        if (minDifference > currentMinDifference) {
+            minDifference = currentMinDifference;
+            result.first = a[i];
+            result.second = b[j];
+        }
+
+        // Move the pointers based on the comparison
+        if (a[i] < b[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    return result;
+}
+
 int main(int argc, const char * argv[]) {
     vector<int> firstArr {23, 5, 10, 17, 30};
     vector<int> secondArr {26, 134, 135, 14, 19};
